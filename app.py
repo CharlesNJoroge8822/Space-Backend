4e53f8fdc4dd99e8f2d2b529cdeb93293805145d
luqman-bashir <uthamin98@gmail.com>
2025-02-18 12:30:26 +0300

    move auth.py from auths to views

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from flask_migrate import Migrate
 from models import db, User, Space, Booking, Payment, Agreement, TokenBlockList  # ✅ Ensure correct import
+from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
 
 # ✅ Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rental.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "your_super_secret_key_here"
+
 
 # ✅ Initialize db and Migrate
 db.init_app(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
+
 
 # ✅ Register Blueprints (Moved to Avoid Circular Imports)
 from views.user_routes import user_bp
@@ -18,12 +24,15 @@ from views.space_routes import space_bp
 from views.bookings import booking_bp
 from views.payments_routes import payment_bp
 from views.agreement_routes import agreement_bp
+from views.auths import auth_bp
 
 app.register_blueprint(user_bp)
 app.register_blueprint(space_bp)
 app.register_blueprint(booking_bp)
 app.register_blueprint(payment_bp)
 app.register_blueprint(agreement_bp)
+app.register_blueprint(auth_bp)
+
 
 
 
